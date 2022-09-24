164a19d9bb21e64d3f17bfe468f63c0eef115526
vinisperandio <vinisperandio13@gmail.com>
2022-09-24 14:59:18 -0300

    commit inicial

---
+++ src/main/java/com/amefastforward/cardapi/CardapiApplication.java
@@ -0,0 +1,13 @@
+package com.amefastforward.cardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amefastforward/cardapi/CardapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amefastforward.cardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
